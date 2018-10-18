4b64e0f5a8c74203bc8e6b26d77251819706ba3c
vvdhout <valentijnvdh@gmail.com>
2018-10-18 14:22:59 +0200

    First commit adding startupBuilder folder

---
+++ startupBuilder/run.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(8000, () => {
+	console.log('App is listening on port 8000');
+})
+
+// Setting the views folder to call files from
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Calling the home.ejs file from the views folder when on / 
+app.get('/', (req, res) => { 
+	res.render('index');
+});
